b6eaaf2decf72eccfc98d6fd41040d2be78621f2
Tiulenchik <ayanamirei793@gmail.com>
2025-05-30 15:55:13 +0300

    Added CRUD operations (temporary database)

---
+++ CSVWork.py
@@ -0,0 +1,48 @@
+import csv
+
+def create_record(filename, record):
+    with open(filename, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(record)
+
+def read_records(filename):
+    all_records = []
+    with open(filename, mode='r', newline='') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            all_records.append(row)
+    return all_records
+
+def update_record(filename, record_id, updated_record):
+    rows = []
+    with open(filename, mode='r', newline='') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row[0] == record_id:
+                rows.append(updated_record)
+            else:
+                rows.append(row)
+
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(rows)
+
+def delete_record(filename, record_id):
+    rows = []
+    with open(filename, mode='r', newline='') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row[0] != record_id:
+                rows.append(row)
+
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(rows)
+
+def is_user_exists(filename, search_id):
+    with open(filename, mode='r', newline='') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if row['username'] == search_id:
+                return True
+    return False
--- GamblingBot.py
+++ GamblingBot.py
@@ -5,31 +5,44 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from aiogram.types import Dice
 
+import CSVWork
+
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=getenv("GAMBLING_BOT_TOKEN"))
+DATA_FILE = "test_users.csv"
+VALUES = ["BAR", "Grapes", "Lemon", "7"]
 dp = Dispatcher()
 
-@dp.message(Command("start"))
-async def command_start(message: types.Message):
-    await message.answer("Hello!")
-
-
-async def main():
-    await dp.start_polling(bot)
 @dp.message()
 async def check_rolls(message: types.Message):
     if isinstance(message.dice, Dice):
         rolled = message.dice
         dice_value = rolled.value
-        VALUES = ["BAR", "Grapes", "Lemon", "7"]
+        
         dice_value -= 1
         result = []
         for _ in range(3):
             result.append(VALUES[dice_value % 4])
             dice_value //= 4
         await asyncio.sleep(3)
-        await message.answer(f"I see {rolled.emoji}. And you rolled {rolled.value}")
         if rolled.emoji == "🎰":
-            await message.answer(f"Slot machine! {result[0]}, {result[1]}, {result[2]}")
+            if rolled.value == 64:
+                pass # add soon score tracking
+@dp.message(Command("add"))
+async def add_user(message: types.Message):
+    print(1)
+    if CSVWork.is_user_exists(DATA_FILE, message.from_user.username):
+        await message.reply(f"Ти вже у статистиці!")
+    else:
+        CSVWork.create_record(DATA_FILE, [message.from_user.username, 0, 0, 0])
+        await message.reply(f"Записав тебе в свою пам'ять. Приємної гри!'")
+
+@dp.message(Command("stats"))
+async def check_stats(message: types.Message):
+    await message.answer(str(CSVWork.read_records(DATA_FILE)))
+
+
+async def main():
+    await dp.start_polling(bot)
 if __name__ == "__main__":
     asyncio.run(main())
